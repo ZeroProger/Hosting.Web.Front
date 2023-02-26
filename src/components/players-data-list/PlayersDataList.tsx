b1b73917fe096f63c567ee49d37ca9cc092aaeaa
Андрей <andrey.official.phone@gmail.com>
2023-02-26 22:08:31 +0500

    complete server players

--- src/components/players-data-list/PlayersDataList.tsx
+++ src/components/players-data-list/PlayersDataList.tsx
@@ -1,5 +1,7 @@
-import { Input } from '@nextui-org/react'
+import { Button, Input } from '@nextui-org/react'
+import clsx from 'clsx'
 import { bannedIps, whiteListUsers } from 'fakeData/users.data'
+import Image from 'next/image'
 import { FC } from 'react'
 
 import { PlayersDataListType } from '@/shared/types/user.types'
@@ -24,23 +26,44 @@ const PlayersDataList: FC<IPlayersDataList> = ({ title, addDataPlaceholder, data
 					{dataType !== PlayersDataListType.BannedIps &&
 						whiteListUsers.map((user) => (
 							<div className={styles.tableItem} key={user.id}>
-								<div className={styles.tableItemData}></div>
-								<div className={styles.tableItemAction}></div>
+								<div className={styles.tableItemData}>
+									<Image src={user.image} alt={`Аватар ${user.name}`} width={32} height={32} />
+									<span>{user.name}</span>
+								</div>
+								<div className={styles.tableItemAction}>
+									<Button className="btn-error">
+										<Icon name="MdDelete" color="#fff" size={32} />
+									</Button>
+								</div>
 							</div>
 						))}
 					{dataType === PlayersDataListType.BannedIps &&
 						bannedIps.map((ip) => (
 							<div className={styles.tableItem} key={ip.ip}>
 								<div className={styles.tableItemData}>
-									<Icon name="TbCurrentLocation" size={24} />
+									<Icon name="TbCurrentLocation" size={32} />
 									<span>{ip.ip}</span>
 								</div>
-								<div className={styles.tableItemAction}></div>
+								<div className={styles.tableItemAction}>
+									<Button className="btn-error">
+										<Icon name="MdDelete" color="#fff" size={32} />
+									</Button>
+								</div>
 							</div>
 						))}
-				</div>
-				<div className={styles.addData}>
-					<Input type="text" placeholder={addDataPlaceholder} />
+					<div className={clsx(styles.tableItem, styles.addData)}>
+						<Input
+							type="text"
+							placeholder={addDataPlaceholder}
+							fullWidth
+							animated={false}
+							shadow={false}
+						/>
+						<Button className={clsx('btn-default', styles.addDataBtn)}>
+							<Icon name="AiOutlinePlus" size={24} />
+							<span>Добавить</span>
+						</Button>
+					</div>
 				</div>
 			</div>
 		</div>
