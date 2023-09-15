c6c14289af683ef6da505ad8cb976b3cec27b654
Andrey Zhiznevsky <andrey.official.phone@gmail.com>
2023-09-15 22:26:07 +0500

    start route urls rewrite

--- src-old/config/url.config.ts
+++ src-old/config/url.config.ts
@@ -2,14 +2,6 @@ import { ISearchModsQuery } from '@/shared/types/curseforge.types'
 
 export const APP_URL = `${process.env.APP_URL}`
 
-export const getServersUrl = (query?: string) => `/servers${query ? `?${query}` : ''}`
-
-export const getPublicServersUrl = (query?: string) => `/public-servers${query ? `?${query}` : ''}`
-
-export const getServerUrl = (slug: string) => `/servers/${slug}/overview`
-
-export const getServerCreateUrl = (query?: string) => `/servers/create${query ? `?${query}` : ''}`
-
 export const getServerOverviewUrl = (slug: string) => `/servers/${slug}/overview`
 
 export const getServerPlayersUrl = (slug: string, category?: string) =>
@@ -32,33 +24,33 @@ export const getServerModSearchUrl = (query?: ISearchModsQuery) => {
 	return `/servers/mods/search${resultQueryString}`
 }
 
-export const getServerModUrl = (modId: string, category?: string) =>
-	`/servers/mods/${modId}${category && category.length > 1 ? `${category}` : ''}`
+// export const getServerModUrl = (modId: string, category?: string) =>
+// 	`/servers/mods/${modId}${category && category.length > 1 ? `${category}` : ''}`
 
-export const getServerModFilesUrl = (modId: string) => `/servers/mods/${modId}/files`
+// export const getServerModFilesUrl = (modId: string) => `/servers/mods/${modId}/files`
 
-export const getServerModImagesUrl = (modId: string) => `/servers/mods/${modId}/images`
+// export const getServerModImagesUrl = (modId: string) => `/servers/mods/${modId}/images`
 
-export const getServerModRelationsUrl = (modId: string) => `/servers/mods/${modId}/relations`
+// export const getServerModRelationsUrl = (modId: string) => `/servers/mods/${modId}/relations`
 
-export const getServerConsoleUrl = (slug: string) => `/servers/${slug}/console`
+// export const getServerConsoleUrl = (slug: string) => `/servers/${slug}/console`
 
-export const getServerLogsUrl = (slug: string) => `/servers/${slug}/logs`
+// export const getServerLogsUrl = (slug: string) => `/servers/${slug}/logs`
 
-export const getServerFilesUrl = (slug: string, path: string = '') =>
-	`/servers/${slug}/files${path}`
+// export const getServerFilesUrl = (slug: string, path: string = '') =>
+// 	`/servers/${slug}/files${path}`
 
-export const getServerBackupsUrl = (slug: string) => `/servers/${slug}/backups`
+// export const getServerBackupsUrl = (slug: string) => `/servers/${slug}/backups`
 
-export const getServerSettingsUrl = (slug: string) => `/servers/${slug}/settings`
+// export const getServerSettingsUrl = (slug: string) => `/servers/${slug}/settings`
 
-export const getServerSoftwareUrl = (slug: string) => `/servers/${slug}/software`
+// export const getServerSoftwareUrl = (slug: string) => `/servers/${slug}/software`
 
-export const getServerVersionsUrl = (slug: string, core: string) =>
-	`${getServerSoftwareUrl(slug)}/${core}`
+// export const getServerVersionsUrl = (slug: string, core: string) =>
+// 	`${getServerSoftwareUrl(slug)}/${core}`
 
-export const getServerVersionUrl = (slug: string, core: string, version: string) =>
-	`${getServerVersionsUrl(slug, core)}/${version}`
+// export const getServerVersionUrl = (slug: string, core: string, version: string) =>
+// 	`${getServerVersionsUrl(slug, core)}/${version}`
 
 export const getTariffUrl = (slug: string) => `/tariffs/${slug}`
 
--- src/shared/config/url.config.ts
+++ src/shared/config/url.config.ts
@@ -1,5 +1,67 @@
 export const ServerUrls = {
+	server: {
+		overview: (slug: string) => `/servers/${slug}/overview`,
+
+		players: (slug: string, category?: string) => `/servers/${slug}/players${category || ''}`,
+
+		console: (slug: string) => `/servers/${slug}/console`,
+
+		logs: (slug: string) => `/servers/${slug}/logs`,
+
+		files: (slug: string) => `/servers/${slug}/files`,
+
+		backups: (slug: string) => `/servers/${slug}/backups`,
+
+		settings: (slug: string) => `/servers/${slug}/settings`,
+
+		software: (slug: string) => `/servers/${slug}/software`,
+
+		versions: (slug: string, core: string) => ServerUrls.server.software(slug) + '/' + core,
+
+		version: (slug: string, core: string, version: string) =>
+			ServerUrls.server.versions(slug, core) + '/' + version,
+	},
+
+	servers: (query?: string) => `/servers${query ? `?${query}` : ''}`,
+
+	publicServers: (query?: string) => `/public-servers${query ? `?${query}` : ''}`,
+
 	createServer(query?: string) {
 		return `/servers/create${query ? `?${query}` : ''}`
 	},
+}
+
+export const ModUrls = {
+	mods: () => '/mods',
+	mod: (id: number | string) => `/mods/${id}`,
+	files: (id: number | string) => `/mods/${id}/files`,
+	images: (id: number | string) => `/mods/${id}/images`,
+	relations: (id: number | string) => `/mods/${id}/relations`,
+}
+
+export const AuthUrls = {
+	signIn: () => '/auth/sign-in',
+	signUp: () => '/auth/sign-up',
+	logout: () => '/auth/logout',
+}
+
+export const ProfileUrls = {
+	profile: () => '/profile',
+	settings: () => '/settings',
+}
+
+export const HelpUrls = {
+	help: () => '/help',
+	feedback: () => '/feedback',
+}
+
+//#TODO: переезд tariffs на subscription model
+export const SubscriptionUrls = {
+	subscriptions: () => '/subscriptions',
+	subscription: (slug: string) => `/subscriptions/${slug}`,
+}
+
+export const GameUrls = {
+	games: () => '/games',
+	game: (gameId: number) => `/games/${gameId}`,
 }
