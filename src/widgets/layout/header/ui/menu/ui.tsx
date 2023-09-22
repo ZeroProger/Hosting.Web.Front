c2d6d65a85e347066be7d6c867326ce603bfe442
Andrey Zhiznevsky <andrey.official.phone@gmail.com>
2023-09-22 12:37:05 +0500

    fix header + burger menu styles

--- src/widgets/layout/header/ui/menu/ui.tsx
+++ src/widgets/layout/header/ui/menu/ui.tsx
@@ -1,9 +1,13 @@
-'use client';
+'use client'
 
-import clsx from 'clsx';
+import clsx from 'clsx'
 import { useStore } from 'effector-react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+
+import { $userServers } from '@/entities/server/model'
+
+import { ServerSelect } from '@/widgets/server/server-select'
 
 import { $headerMenu } from '../../store'
 
@@ -12,11 +16,15 @@ import styles from './styles.module.scss'
 
 export function Menu() {
 	const { isHeaderMenuOpen } = useStore($headerMenu)
+	const userServers = useStore($userServers)
 	const pathname = usePathname()
 
 	return (
 		<div className={clsx(styles.container, { [styles.open]: isHeaderMenuOpen })}>
 			<nav className={styles.content}>
+				<div className={styles.serverSelect}>
+					<ServerSelect servers={userServers} />
+				</div>
 				<ul className={styles.list}>
 					{menuItems.map((menuItem) => (
 						<Link key={menuItem.label} href={menuItem.url} className={styles.item}>
