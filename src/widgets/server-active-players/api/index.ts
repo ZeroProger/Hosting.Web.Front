f67ea802768d7c0a4b73f78f5e9f52492cd380aa
Andrey Zhiznevsky <andrey.official.phone@gmail.com>
2023-09-29 11:10:55 +0500

    some logs for polling data

--- src/widgets/server-active-players/api/index.ts
+++ src/widgets/server-active-players/api/index.ts
@@ -1,5 +1,6 @@
 import { activePlayers } from '@/shared/$fake-data$/players.data'
 
 export function getActivePlayers(gameServerHash: string) {
+	console.log('polling serverActivePlayers...')
 	return activePlayers
 }
