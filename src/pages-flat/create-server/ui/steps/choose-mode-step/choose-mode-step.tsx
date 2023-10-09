f243d728acb1aca27dda2cdb78dd3ee9e21f28fd
Andrey Zhiznevsky <andrey.official.phone@gmail.com>
2023-10-09 12:12:37 +0500

    choose-mode-step fixes, 3/5 steps complete

--- src/pages-flat/create-server/ui/steps/choose-mode-step/choose-mode-step.tsx
+++ src/pages-flat/create-server/ui/steps/choose-mode-step/choose-mode-step.tsx
@@ -5,6 +5,7 @@ import { Heading } from '@/shared/ui/heading'
 import { useStore } from 'effector-react'
 import { $serverCreateFormStep, setFormStep } from '../../../model'
 import { ECreateServerFormStep } from '../../../types'
+import styles from './styles.module.scss'
 
 export function ChooseModeStep() {
 	const formStep = useStore($serverCreateFormStep)
@@ -20,27 +21,23 @@ export function ChooseModeStep() {
 	if (formStep !== ECreateServerFormStep.CHOOSE_MODE) return null
 
 	return (
-		<div className="w-full max-w-lg flex flex-col items-center gap-2">
+		<div className={styles.container}>
 			<Heading>Помочь с выбором тарифа?</Heading>
-			<div className="flex flex-col items-center justify-center gap-4">
+			<div className={styles.buttons}>
 				<Button
 					variant="outline"
 					onClick={easyModeClick}
-					className="h-auto flex flex-col gap-2 flex-1"
+					className="h-auto flex flex-col gap-2 border-primary/70 hover:border-primary"
 				>
-					<span className="text-xl font-bold">Да, мне нужна помощь с выбором</span>
-					<p className="text-lg text-foreground/70">
+					<span className={styles.title}>Да, мне нужна помощь с выбором</span>
+					<p className={styles.description}>
 						Мы предложим вам тариф на основе ваших потребностей: сколько игроков будет играть на
 						сервере, с какими модами вы будете играть (а может без модов?).
 					</p>
 				</Button>
-				<Button
-					variant="outline"
-					onClick={advanceModeClick}
-					className="h-auto flex flex-col gap-2 flex-1"
-				>
-					<span className="text-xl font-bold">Нет, я сам выберу конфигурацию сервера</span>
-					<p className="text-lg text-foreground/70">
+				<Button variant="outline" onClick={advanceModeClick} className="h-auto flex flex-col gap-2">
+					<span className={styles.title}>Нет, я сам выберу конфигурацию сервера</span>
+					<p className={styles.description}>
 						Не будем задавать никаких уточняющих вопросов, сразу перейдём к выбору CPU, RAM и других
 						характеристик сервера.
 					</p>
